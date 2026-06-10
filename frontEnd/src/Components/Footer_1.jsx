13ff9d649362fc7d238e0264a5126e71a5e8942e
omarfaruk-dotcom <omarfaruk122019@gmail.com>
2026-06-10 23:29:59 +0600

    secons commit to check

--- frontEnd/src/Components/Footer_1.jsx
+++ frontEnd/src/Components/Footer_1.jsx
@@ -4,7 +4,7 @@ export default function Footer_1() {
     return (
         <footer>
             <div>
-                <h1 className="text-2xl font-bold text-primary">CONB2B</h1>
+                <h1 className="text-2xl font-bold text-primary">CONB2B this has been updated</h1>
             </div>
         </footer>
     )
